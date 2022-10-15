925332fb203a8a7dc1997b9c4dfb94764fe8d915
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-10-15 18:21:31 +0300

    fix incorrect indentation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use roxy::{scanner::*, parser::Parser, interpreter::Interpreter};
+use roxy::{interpreter::Interpreter, parser::Parser, scanner::*};
 
 fn main() {
     let source = r#"print 1 + 2;"#.to_string();
-        let mut scanner = Scanner::new(source);
-        let tokens = scanner.scan_tokens();
-        let mut parser = Parser::new(tokens.clone());
-        let stmts = parser.parse();
-        Interpreter::interpret(stmts);
+    let mut scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+    let mut parser = Parser::new(tokens.clone());
+    let stmts = parser.parse();
+    Interpreter::interpret(stmts);
 }
